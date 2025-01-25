2f83d141c1761e013bdc9cc9960911ec5340b054
Slok9931 <148342312+Slok9931@users.noreply.github.com>
2025-01-26 00:32:09 +0530

    Login bug fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useFetchData } from "@/core/api/apiConfig";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -9,12 +8,27 @@ interface Props {
 }
 
 export default function Login() {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const [credentials, setCredentials] = useState({ phoneNumber: "", password: "" });
+
+  const { data: payload, refetch, isError, error } = useQuery<Props>({
+    queryKey: ["auth", credentials],
+    queryFn: () =>
+      fetch(`http://10.50.49.216:5000/v1/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      }).then((res) => {
+        console.log(credentials);
+        console.log(res);
+        return res.json();
+      }),
+    enabled: false, // Disable automatic fetching
+  });
 
   const handleOnClick = (e: React.FormEvent) => {
     e.preventDefault();
-    // refetch(); // Trigger the query manually
-    useFetchData({ url: '/v1/auth/login', queryKey: ["auth", credentials], method: "POST", body: credentials });
+console.log(credentials);
+    refetch(); // Trigger the query manually
   };
 
   return (
@@ -23,13 +37,13 @@ export default function Login() {
       <form onSubmit={handleOnClick}>
         <div>
           <input
-            name="username"
-            placeholder="Username"
+            name="phoneNumber"
+            placeholder="Phone Number"
             required
-            autoComplete="username"
-            value={credentials.username}
+            autoComplete="phoneNumber"
+            value={credentials.phoneNumber}
             onChange={(e) =>
-              setCredentials({ ...credentials, username: e.target.value })
+              setCredentials({ ...credentials, phoneNumber: e.target.value })
             }
           />
         </div>
@@ -49,12 +63,12 @@ export default function Login() {
         <div>
           <button type="submit">Submit</button>
         </div>
-        {/* <div>
+        <div>
           {payload && <pre>{JSON.stringify(payload, null, 2)}</pre>}
         </div>
         {isError && (
           <p style={{ color: "red" }}>{error?.message || "Invalid credentials"}</p>
-        )} */}
+        )}
       </form>
     </div>
   );
